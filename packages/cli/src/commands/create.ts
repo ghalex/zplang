bfdfb2a5785b29c234363e7858af4052876b5921
Alexandru <ghalex@gmail.com>
2024-05-17 12:49:42 +0000

    add install dependencies

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -11,7 +11,7 @@ const program = new Command('create')
 const createProject = async (name: string) => {
   if (fs.existsSync(name)) {
     console.log(`${clc.red('✖ Error: ')} Path ${name} already exists and is not an empty directory`)
-    return
+    return false
   }
 
   const spinner = ora(`Creating project ${clc.bold.cyanBright(name)}`).start()
@@ -22,10 +22,26 @@ const createProject = async (name: string) => {
     spinner.succeed()
     console.log(`${clc.green('✔ Success: ')} Project ${clc.bold.cyanBright(name)} created successfully`)
 
+    return true
   } else {
-
     spinner.fail()
     console.log(`${clc.red('✖ Error: ')} Project ${name} could not be created`)
+
+    return false
+  }
+}
+
+const installDependencies = async (projectName:string, pkName: string) => {
+  shell.cd(projectName)
+  const spinner = ora(`Installing dependencies`).start()
+  const res = shell.exec(`${pkName} install`, { silent: true })
+
+  if (res.code === 0) {
+    spinner.succeed()
+    console.log(`${clc.green('✔ Success: ')} Dependencies installed successfully`)
+  } else {
+    spinner.fail()
+    console.log(`${clc.red('✖ Error: ')} Dependencies could not be installed`)
   }
 }
 
@@ -36,6 +52,7 @@ export default () => {
     .argument('name', 'project name')
     .action(async (projectName, opts) => {
         // const { name } = await prompts({ type: 'text', name: 'name', message: 'Enter project name', initial: projectName })
+
         const { confirm } = await prompts({
           type: 'toggle',
           name: 'confirm',
@@ -47,6 +64,31 @@ export default () => {
 
         if (confirm) {
           const data = await createProject(projectName)
+          if (!data) return
+
+          const { mustInstallDependencies } = await prompts({
+            type: 'toggle',
+            name: 'mustInstallDependencies',
+            message: `Would you like to install dependencies?`,
+            initial: true,
+            active: 'yes',
+            inactive: 'no'
+          })
+
+          if (mustInstallDependencies) {
+            const { pkName } = await prompts({
+              type: 'select',
+              name: 'pkName',
+              message: `Select a package manager?`,
+              choices: [
+                { title: 'npm', value: 'npm' },
+                { title: 'yarn', value: 'yarn' },
+                { title: 'pnpm', value: 'pnpm' }
+              ]
+            })
+
+            await installDependencies(projectName, pkName)
+          }
         }
 
     })
